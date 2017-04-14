e7b29cb57772237d14c89d3ffef450ff3b270df6
unknown <Administrator@NBADL1610012.corp.ais900.org>
2017-04-14 17:05:25 +0700

    add title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import 'font-awesome/css/font-awesome.css';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +13,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          FBB - Billboard.
         </p>
       </div>
     );
